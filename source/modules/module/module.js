436f9eb09d214ed71ca2802c2c00bc31c90fa7cc
Natasha Banegas <natasha.banegas@gmail.com>
2012-05-08 18:32:14 -0400

    Updated file structure. Added module template.

---
+++ source/modules/module/module.js
@@ -0,0 +1,29 @@
+﻿/*!
+* File name:   module.js
+* Description: Description of the module
+* Version:     1.0
+*/
+
+// Rename SITE to your global namespace
+var SITE = SITE || {};
+
+// Rename "modulename" to the name of your module
+SITE.modulename = (function (my, $) {
+	
+	var el;
+	
+	my.init = function(element) {
+		el = element;
+		
+	}
+	
+	return my;
+	
+} (SITE.modulename || {}, jQuery));
+
+jQuery(document).ready(function ($) {
+	var el = $('#modulename');
+	if (el.length > 0) {
+		SITE.modulename.init(el);
+	}
+});
