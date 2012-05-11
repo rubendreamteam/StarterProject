3ef2b25ebddf3b0c8f4fe4cd3c11926714210a75
Carlos Rodriguez <me@carlosrodriguez.us>
2012-05-10 21:14:01 -0400

    Updated Paul Irish wrapper. It passes jshint now

--- grunt.js
+++ grunt.js
@@ -7,7 +7,7 @@ module.exports = function(grunt) {
       files: ['test/**/*.html']
     },
     lint: {
-      files: ['grunt.js', 'source/ui/*/*.js', , 'source/module/*.js' 'source/test/**/*.js']
+      files: ['grunt.js', 'source/ui/*/*.js', 'source/modules/**/*.js', 'source/test/**/*.js']
     },
     watch: {
       files: '<config:lint.files>',
@@ -15,17 +15,18 @@ module.exports = function(grunt) {
     },
     jshint: {
       options: {
-        curly: true,
+        /*curly: true,
         eqeqeq: true,
         immed: true,
         latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
+        newcap: true,*/
+        noarg: false, // Allow the arguments.callee = arguments.callee.caller; for Paul Irish console wrapper
+        /*sub: true,
         undef: true,
         boss: true,
         eqnull: true,
-        browser: true
+        browser: true,*/
+        jquery: true
       }
     },
     uglify: {}
