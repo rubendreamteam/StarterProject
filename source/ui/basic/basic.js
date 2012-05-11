ea97598cac9ea9c13fc996ef86781fc232f6b7b9
Carlos Rodriguez <me@carlosrodriguez.us>
2012-05-10 21:41:23 -0400

    Fixed the console script on basic to pass jshint

--- source/ui/basic/basic.js
+++ source/ui/basic/basic.js
@@ -15,7 +15,5 @@ window.log = function(){
 };
 
 // make it safe to use console.log always
-(function (b) { function c() { } for (var d = "assert,clear,count,debug,dir,dirxml,error,exception,firebug,group,groupCollapsed,groupEnd,info,log,memoryProfile,memoryProfileEnd,profile,profileEnd,table,time,timeEnd,timeStamp,trace,warn".split(","), a; a = d.pop(); ) { b[a] = b[a] || c; } })((function () {
-	try
-{ console.log(); return window.console; } catch (err) { return window.console = {}; } 
-})());
+(function(a){function b(){}for(var c="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn".split(","),d;!!(d=c.pop());){a[d]=a[d]||b;}})
+(function(){try{console.log();return window.console;}catch(a){return (window.console={});}}());
